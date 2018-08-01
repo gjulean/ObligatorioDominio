c58905358057784fcae93eb04b38146699fdb538
Usuario <Usuario@DESKTOP-RGNSVH0>
2018-08-01 15:59:48 -0300

    cambio chef

--- ObligatorioDominio/Chef.cs
+++ ObligatorioDominio/Chef.cs
@@ -9,7 +9,7 @@ namespace ObligatorioDominio
    public class Chef
     {
 
-       
+       //Cambio Git!!!!
         #region Atributos
         private string documento;
         private TipoDocumento tipoDocumento;
